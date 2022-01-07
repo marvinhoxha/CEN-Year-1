36edb8075f5cbd163483b40e5aa73c8ce56795c2
Marvin Hoxha <64254074+marvinhoxha@users.noreply.github.com>
2022-01-07 17:19:56 +0100

    Update main.c

--- Semester 1/classwork week 2 1/main.c
+++ Semester 1/classwork week 2 1/main.c
@@ -1,23 +1,17 @@
+/*Exercise to find the sum of tow numbers*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-   int a,b,sum;
+   int a,b,sum=0;
    printf("Enter first num: ");
    scanf("%d",&a);
    printf("enter second num: ");
    scanf("%d",&b);
-   if (a>b)
-   {
-      sum=a+b;
-   }
-   else
-   {
-      sum=a-b;
-   }
 
-
+    sum=a+b;
 
     printf("sum of two num is: %d",sum);
     return 0;
