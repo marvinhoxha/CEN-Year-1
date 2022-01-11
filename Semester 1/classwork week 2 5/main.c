c634dcbc106fbb518f4f564f6aad7ed8c1ba8fc2
Marvin Hoxha <marvin.hoxha1@gmail.com>
2022-01-11 10:19:51 +0100

    classwork week 2 ex 5

---
+++ Semester 1/classwork week 2 5/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int n1,n2,n3;
+  float x;
+    scanf("%d%d%d",&n1,&n2,&n3);
+    x=(n1+n2+n3)/3.0;
+    if(x>=59.5)
+        {
+    printf("Passed");
+        }
+    else
+    {
+      printf("Failed");
+    }
+
+    return 0;
+}
