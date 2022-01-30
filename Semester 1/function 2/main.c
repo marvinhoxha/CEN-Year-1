7147d5a118defdd86f1aae478f56e879ec9d8b18
Marvin Hoxha <64254074+marvinhoxha@users.noreply.github.com>
2022-01-30 22:19:35 +0100

    Update main.c

--- Semester 1/function 2/main.c
+++ Semester 1/function 2/main.c
@@ -1,4 +1,6 @@
 //mhoxha20
+/*exercise that reads from the input.txt file n->nr of numbers k->the number to use in the rand() function and 
+then makes a loop n times and adds random numbers from 0 to k-1 to the numrand summ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
